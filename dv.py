18114235ce7600004724021a67f4d00427c1a160
yeg12 <yeg@nycstudents.net>
2026-05-01 20:57:30 +0000

    bfefw

--- dv.py
+++ dv.py
@@ -3,13 +3,15 @@
 
 import requests
 
-def money(currency):
-    response = requests.get(f"https://v6.exchangerate-api.com/v6/ff65fb42666ad879f87f0b1f/latest/USD")
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    data = response.json()
-    user = input ('what currency u want USD into ? What 3letters  ')
-    if user in data:
-      print (data)
-money("USD")
+currency = input("Enter currency (e.g. EUR): ").upper()
+
+api = "https://v6.exchangerate-api.com/v6/ff65fb42666ad879f87f0b1f/latest/USD"
+response = requests.get(api)
+
+data = response.json()
+rates = data["conversion_rates"]
+
+if currency in rates:
+    print("1 USD =", rates[currency], currency)
+else:
+    print("Invalid currency")
