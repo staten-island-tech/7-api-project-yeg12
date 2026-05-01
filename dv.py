7808dc2829360109b3662c70ffff458382b61539
yeg12 <yeg@nycstudents.net>
2026-05-01 09:55:08 -0400

    Update dv.py

--- dv.py
+++ dv.py
@@ -1,12 +1,15 @@
+# Your API Key: ff65fb42666ad879f87f0b1f
+# Example Request: https://v6.exchangerate-api.com/v6/ff65fb42666ad879f87f0b1f/latest/USD
+
 import requests
 
 def money(currency):
     response = requests.get(f"https://v6.exchangerate-api.com/v6/ff65fb42666ad879f87f0b1f/latest/USD")
     if response.status_code != 200:
         print("Error fetching data!")
-        
+        return None
     data = response.json()
-    User = input ("what currency u want 1 USD in(the letters)")
-    for d in data:
-      if User in d:
-        print(d)
+    user = input ('what currency u want USD into ? What 3letters  ')
+    if user in data:
+      print (data)
+money("USD")
