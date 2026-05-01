febfc26807321e728dd2dec2866aef4bbcb8f60f
yeg12 <yeg@nycstudents.net>
2026-05-01 09:42:10 -0400

    Update dv.py

--- dv.py
+++ dv.py
@@ -1,18 +1,12 @@
-from balldontlie import BalldontlieAPI
+import requests
 
-api = BalldontlieAPI(api_key="fa43702b-b869-48a1-a860-e48771ac632b")
-teams = api.nba.teams.list()
-{
-  "data": [
-    {
-      "id":1,
-      "conference":"East",
-      "division":"Southeast",
-      "city":"Atlanta",
-      "name":"Hawks",
-      "full_name":"Atlanta Hawks",
-      "abbreviation":"ATL"
-    },
-    ...
-  ]
-}
+def money(currency):
+    response = requests.get(f"https://v6.exchangerate-api.com/v6/ff65fb42666ad879f87f0b1f/latest/USD")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        
+    data = response.json()
+    User = input ("what currency u want 1 USD in(the letters)")
+    for d in data:
+      if User in d:
+        print(d)
