3aebdede9bbb7e9a95b13c9c4968d9f1b73dc0a0
yeg12 <yeg@nycstudents.net>
2026-05-19 09:43:36 -0400

    fe

--- dv.py
+++ dv.py
@@ -1,15 +1,15 @@
 # Your API Key: ff65fb42666ad879f87f0b1f
 # Example Request: https://v6.exchangerate-api.com/v6/ff65fb42666ad879f87f0b1f/latest/USD
 import requests
-currency = input("Enter currency (like EUR): ")
+cur = input("Enter currency like EUR ")
 
-api = "https://v6.exchangerate-api.com/v6/ff65fb42666ad879f87f0b1f/latest/USD"
-response = requests.get(api)
+key = "https://v6.exchangerate-api.com/v6/ff65fb42666ad879f87f0b1f/latest/USD"
+request = requests.get(key)
 
-data = response.json()
-rates = data["conversion_rates"]
+data = request.json()
+rate = data["conversion_rates"]
 
-if currency in rates:
-    print("1 USD =", rates[currency])
+if cur in rate:
+    print("1 usd =", rate[cur])
 else:
     print("Invalid currency")
